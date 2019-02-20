acf24f07d30114e5aa2f787b2bf6e679078a1c14
MarcNeundörfer\Marc Neundörfer <marc.neundoerfer@gmx.de>
2019-02-20 14:00:53 +0100

    Added som Tests

--- src/test/java/TestServerSocket.java
+++ src/test/java/TestServerSocket.java
@@ -1,10 +1,21 @@
+import org.coolimc.ProxyServer.HttpProxy.HttpProxyServer;
+import org.coolimc.ProxyServer.SocksProxy.SocksProxyServer;
+
 import org.junit.jupiter.api.Test;
 
 public class TestServerSocket
 {
     @Test
-    void justAnExample()
+    void testHttpProxyStart()
     {
+        //Test the start of the HttpServer
+        new HttpProxyServer(0).listen();
+    }
 
+    @Test
+    void testSocksProxyStart()
+    {
+        //Test the start of the SocksServer
+        new SocksProxyServer(0).listen();
     }
 }
